1256222e4abadd2a2b40da63e27ecce76b40740c
Alexey1995200 <39001656+Alexey1995200@users.noreply.github.com>
2026-03-06 08:58:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Spotify Mobile + AdBlocker + Mute
+// @version      2025-11-17.1/
 // @description  Mobile-style Spotify layout + blocks all ads + mutes Spotify ads automatically.
 // @author       Anonymous
 // @match        https://open.spotify.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=spotify.com
 // @grant        none
-// @updateURL https://raw.githubusercontent.com/Alexey1995200/MobileSpot/refs/heads/main/script.js
+// @downloadURL https://update.greasyfork.org/scripts/558091/Spotify%20Mobile%20%2B%20AdBlocker%20%2B%20Mute.user.js
+// @updateURL https://update.greasyfork.org/scripts/558091/Spotify%20Mobile%20%2B%20AdBlocker%20%2B%20Mute.meta.js
 // @updateURLorig https://update.greasyfork.org/scripts/558091/Spotify%20Mobile%20%2B%20AdBlocker%20%2B%20Mute.meta.js
 // ==/UserScript==
+
 
 (function () {
     'use strict';
@@ -21,34 +24,80 @@
     if (!isMobile()) {
         const style = document.createElement('style');
         style.textContent = `
-        #global-nav-bar { margin-bottom: 0.5rem; margin-top: 0.5rem; }
-        #global-nav-bar > div:nth-of-type(1) > a,
-        [data-testid="upgrade-button"],
-        [href="/download"],
-        [data-testid="friend-activity-button"] { display: none!important; }
-        #context-menu > ul > :last-child { display: none!important; }
-        [data-testid="user-widget-menu"] > ul > li:nth-child(8),
-        [data-testid="user-widget-menu"] > ul > li:nth-child(9),
-        [data-testid="user-widget-menu"] > ul > li:nth-child(10) { display: none!important; }
-        body { min-width: unset!important; }
-        #main > div { --panel-gap: 0px!important; }
-        #Desktop_LeftSidebar_Id { display: none!important; }
-        #main > div > div:nth-of-type(2) > div:nth-of-type(7) { display: none!important; }
-        [data-testid="now-playing-bar"] { min-width: unset!important; }
-        [data-testid="now-playing-bar"] > div > div:nth-of-type(1),
-        [data-testid="now-playing-bar"] > div > div:nth-of-type(3) { display: none!important; }
-        [data-testid="now-playing-bar"] > div > div:nth-of-type(2) { max-width: unset!important; width: 90%!important; }
-        [data-testid="now-playing-bar"] > div { justify-content: space-around!important; }
-        [data-testid="footer-div"] { display: none!important; }
-       // [data-testid="tracklist-row"] > div:nth-child(2) > :not(:last-child) { display: none!important; }  
-       // [data-testid="tracklist-row"] > div:last-child > :first-child { display: none!important; }
-          [data-testid="tracklist-row"] button[data-testid="more-button"] {
-        opacity: 1 !important;
-        visibility: visible !important;
-        pointer-events: auto !important;
-        }show more visible
-        .os-scrollbar-track { display: none!important; }
-        `;
+#global-nav-bar { margin-bottom: 0.5rem !important; margin-top: 0.5rem !important; }
+
+#global-nav-bar > div:nth-of-type(1) > a,
+[data-testid="upgrade-button"],
+[href="/download"],
+[data-testid="friend-activity-button"] { display: none !important; }
+
+#context-menu > ul > :last-child { display: none !important; }
+
+[data-testid="user-widget-menu"] > ul > li:nth-child(8),
+[data-testid="user-widget-menu"] > ul > li:nth-child(9),
+[data-testid="user-widget-menu"] > ul > li:nth-child(10) { display: none !important; }
+
+body { min-width: unset !important; }
+
+#main > div { --panel-gap: 0px !important; }
+
+#Desktop_LeftSidebar_Id { display: none !important; }
+
+#main > div > div:nth-of-type(2) > div:nth-of-type(7) { display: none !important; }
+
+/* ---------------- NOW PLAYING BAR LAYOUT ---------------- */
+
+[data-testid="now-playing-bar"] {
+    min-width: unset !important;
+    height: 196px !important;
+    display: flex !important;
+    justify-content: space-between !important;
+    width: 100% !important;
+}
+
+[data-testid="now-playing-bar"] > div {
+    display: flex !important;
+    flex-direction: column !important;
+}
+
+/* верхній та нижній ряд */
+
+[data-testid="now-playing-bar"] > div > div:nth-of-type(1),
+[data-testid="now-playing-bar"] > div > div:nth-of-type(3) {
+    width: 90% !important;
+    padding: 16px 0 !important;
+}
+
+/* центральний блок */
+
+[data-testid="now-playing-bar"] > div > div:nth-of-type(2) {
+    max-width: unset !important;
+    width: 100% !important;
+    justify-content: space-between !important;
+}
+
+/* footer */
+
+[data-testid="footer-div"] { display: none !important; }
+
+/* ---------------- TRACKLIST FIX ---------------- */
+
+[data-testid="tracklist-row"] > div:nth-child(2) > :not(:last-child) { display: none !important; }
+
+[data-testid="tracklist-row"] > div:last-child > :first-child { display: none !important; }
+
+/* SHOW MORE BUTTON ALWAYS VISIBLE */
+
+[data-testid="tracklist-row"] button[data-testid="more-button"] {
+    opacity: 1 !important;
+    visibility: visible !important;
+    pointer-events: auto !important;
+}
+
+/* scrollbar */
+
+.os-scrollbar-track { display: none !important; }
+`;
         document.head.appendChild(style);
 
         const observer = new MutationObserver((mutations, obs) => {
